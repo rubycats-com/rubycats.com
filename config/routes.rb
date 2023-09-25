6f5f260357660382d44be286356198ba0c52ffd1
Yuriy Kirillov <ykirillov@icloud.com>
2023-09-25 23:00:56 +0400

    add tabler

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   get "/up", to: proc { [200, {}, ["OK"]] }
 
   # Defines the root path route ("/")
-  # root "articles#index"
-  root to: proc { [200, {}, ["<=++= RubyCats =++=>"]] }
+  root "pages#index"
 end
