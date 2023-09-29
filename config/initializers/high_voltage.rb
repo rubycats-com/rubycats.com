1aba2d16f44240647a0476e9d535705fbf2624b0
Yuriy Kirillov <ykirillov@icloud.com>
2023-09-29 18:05:15 +0400

    RouteDrawers

--- config/initializers/high_voltage.rb
+++ config/initializers/high_voltage.rb
@@ -1,10 +1,14 @@
-HighVoltage.configure do |config|
-  config.home_page = "home"
-  config.route_drawer = OpenStruct.new(
-    match_attributes: {
+class RouteDrawers
+  def self.match_attributes
+    {
       "/#{HighVoltage.content_path}*id" => "pages#show",
       :as => :page,
       :format => false
     }
-  )
+  end
+end
+
+HighVoltage.configure do |config|
+  config.home_page = "home"
+  config.route_drawer = RouteDrawers
 end
