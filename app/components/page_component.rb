046573ffd6d842025bd6e7125515f62e913f3547
Yuriy Kirillov <ykirillov@icloud.com>
2023-09-29 23:47:22 +0400

    pages github url fix

--- app/components/page_component.rb
+++ app/components/page_component.rb
@@ -13,7 +13,7 @@ class PageComponent < ApplicationComponent
         <div class="row">
           <div class="col-9 mt-2">
             <span>Дополни статью, создав в нее</span>
-            <a href="https://github.com/rubycats-com/rubycats/tree/main/app/views/articles" target="_blank">Pull Request!</a>
+            <a href="https://github.com/rubycats-com/rubycats/tree/main/app/views/pages" target="_blank">Pull Request!</a>
           </div>
           <div class="col-3 text-end">
             <%= render Page::LikeComponent.new(page: page, user: user) if page %>
