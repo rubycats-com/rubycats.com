93940563a989ca1d0ec978dbdc754b01b3cf8310
Yuriy Kirillov <ykirillov@icloud.com>
2023-09-29 17:42:25 +0400

    st

--- config/initializers/high_voltage.rb
+++ config/initializers/high_voltage.rb
@@ -2,7 +2,7 @@ HighVoltage.configure do |config|
   config.home_page = "home"
   config.route_drawer = OpenStruct.new(
     match_attributes: {
-      "/#{HighVoltage.content_path}*id" => 'pages#show',
+      "/#{HighVoltage.content_path}*id" => "pages#show",
       :as => :page,
       :format => false
     }
