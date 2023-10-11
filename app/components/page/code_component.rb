faa3e59e0c5d44fb246e9b3c7f69e2cb0f635936
Yuriy Kirillov <ykirillov@icloud.com>
2023-10-11 14:56:41 +0200

    asdf

--- app/components/page/code_component.rb
+++ app/components/page/code_component.rb
@@ -1,9 +1,13 @@
 class Page::CodeComponent < ApplicationComponent
+  option :title, optional: true
   param :code
 
   erb_template <<~ERB
     <div class="language-plaintext highlighter-rouge">
       <div class="highlight">
+        <% if title %>
+          <code class="my-0"><%= title %></code>
+        <% end %>
         <pre class="highlight text-black" style="background: #f6f8fb; border: 1px solid #e4e5e6"><code><%= code_content %></code></pre>
       </div>
     </div>
